d742532b6a0a7d535dee976939affcc66591b770
robiulxx <86838778+robiulxx@users.noreply.github.com>
2025-09-20 12:56:18 +0600

    botConfig.js

--- config/botConfig.js
+++ config/botConfig.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
 module.exports = {
-  ADMIN_UID: parseInt(process.env.ADMIN_UID),      // তোমার Telegram UID
-  ADMIN_USERNAME: process.env.ADMIN_USERNAME       // তোমার Telegram username (without @)
+  ADMIN_UID: parseInt(process.env.5219109506),      // তোমার Telegram UID
+  ADMIN_USERNAME: process.env.rs24bd       // তোমার Telegram username (without @)
 };
